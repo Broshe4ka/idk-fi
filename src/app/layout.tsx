5bca2189f18fe8ad59d8112930ce3558b86bea96
Broshe4ka <gorelovsergey13@gmail.com>
2024-08-04 15:53:05 +0600

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "../styles/global.scss";
+
+export const metadata: Metadata = {
+  title: "idk-fi",
+  description: "Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="body">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>App could be here :D</h1>
+    </>
+  );
+}
